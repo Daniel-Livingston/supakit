88491da696b2132a0dcc2009d4db6f3f2097da21
Daniel Livingston <contact@daniellivingston.net>
2023-03-13 12:15:18 -0700

    Customize Seo component

---
+++ src/lib/internal/Seo.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Seo } from '@svelteuidev/core';
+	import type { SeoProps } from '@svelteuidev/core';
+
+	interface $$Props extends SeoProps {
+		titleTemplate?: string;
+	}
+
+	export let titleTemplate = '%t% | Supakit';
+</script>
+
+<Seo {titleTemplate} {...$$props} />

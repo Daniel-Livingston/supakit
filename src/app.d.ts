403876638e69832f08eccef081798583407d4441
Daniel Livingston <contact@daniellivingston.net>
2023-03-13 16:53:24 -0700

    Set up Supabase client in server hook

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,16 @@
+import type { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			supabase: ReturnType<typeof createSupabaseServerClient>;
+			getSession: () => Promise<
+				ReturnType<ReturnType<typeof createSupabaseServerClient>['auth']['getSession']>
+			>['data']['session'];
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
---
+++ src/hooks.server.ts
@@ -0,0 +1,24 @@
+import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
+import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+	event.locals.supabase = createSupabaseServerClient({
+		supabaseKey: PUBLIC_SUPABASE_KEY,
+		supabaseUrl: PUBLIC_SUPABASE_URL,
+		event
+	});
+
+	event.locals.getSession = async () => {
+		const {
+			data: { session }
+		} = await event.locals.supabase.auth.getSession();
+		return session;
+	};
+
+	return resolve(event, {
+		filterSerializedResponseHeaders(header) {
+			return header === 'content-range';
+		}
+	});
+};

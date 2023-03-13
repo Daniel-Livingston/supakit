c4b0a06a6d4369f0311939d0684d2d051e0da3aa
Daniel Livingston <contact@daniellivingston.net>
2023-03-13 12:23:43 -0700

    Add basic Navbar component

---
+++ src/lib/internal/Navbar.svelte
@@ -0,0 +1,32 @@
+<header>
+	<nav>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/signup">Signup</a></li>
+			<li><a href="/login">Login</a></li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		border-bottom: 1px solid #eee;
+		position: sticky;
+	}
+
+	ul {
+		display: flex;
+		justify-content: center;
+		gap: 1rem;
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	a {
+		color: #000;
+		display: block;
+		padding: 0.75rem;
+		text-decoration: none;
+	}
+</style>
--- src/lib/internal/index.ts
+++ src/lib/internal/index.ts
@@ -1,1 +1,2 @@
+export { default as Navbar } from './Navbar.svelte';
 export { default as Seo } from './Seo.svelte';

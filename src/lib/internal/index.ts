d72bd74307e45d4a7f1b9e2f59b2eda5297f92fb
Daniel Livingston <contact@daniellivingston.net>
2023-03-13 13:12:27 -0700

    Add basic EmailInput component

--- src/lib/internal/index.ts
+++ src/lib/internal/index.ts
@@ -1,3 +1,4 @@
+export { default as EmailInput } from './EmailInput.svelte';
 export { default as Navbar } from './Navbar.svelte';
 export { default as PasswordInput } from './PasswordInput.svelte';
 export { default as Seo } from './Seo.svelte';

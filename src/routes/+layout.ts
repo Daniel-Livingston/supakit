db8a97d5ad46f6d16ae30060df47e1cf1587fa87
Daniel Livingston <contact@daniellivingston.net>
2023-03-13 18:04:14 -0700

    Add the foundation for the Supabase integration (#2)
    * Add supabase-js package
    
    * Refactor validation functions into separate file
    
    * Switch to specific sveltekit supabase package
    
    * Set up Supabase client in server hook
    
    * Add load function to pass sessions
    
    * Add function to keep page store refreshed
    
    * Add supabase package for types
    
    * Update types to be more simple
    
    * Add a simple check to see if the user is logged in

---
+++ src/routes/+layout.ts
@@ -0,0 +1,19 @@
+import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
+import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
+
+export const load = async ({ fetch, data, depends }) => {
+	depends('supabase:auth');
+
+	const supabase = createSupabaseLoadClient({
+		supabaseKey: PUBLIC_SUPABASE_KEY,
+		supabaseUrl: PUBLIC_SUPABASE_URL,
+		event: { fetch },
+		serverSession: data.session
+	});
+
+	const {
+		data: { session }
+	} = await supabase.auth.getSession();
+
+	return { supabase, session };
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
 	import { Container } from '@svelteuidev/core';
 	import { Seo } from '$lib/internal';
+
+	export let data;
+	$: ({ session } = data);
 </script>
 
 <Seo title="Home" />
 
 <Container>
 	<h1>Home</h1>
+	<div style="text-align:center">
+		{#if session}
+			<p>Welcome, {session.user.email}!</p>
+		{:else}
+			<p>You are not logged in!</p>
+		{/if}
+	</div>
 </Container>
 
 <style>

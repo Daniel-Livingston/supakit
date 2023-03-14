2cfd10ad706dfa3218bcaa7dfbffa0bda6032fa0
Daniel Livingston <contact@daniellivingston.net>
2023-03-13 17:02:12 -0700

    Add function to keep page store refreshed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
 	import { Navbar } from '$lib/internal';
+
+	export let data;
+	$: ({ supabase } = data);
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange(() => {
+			invalidate('supabase:auth');
+		});
+
+		return () => subscription.unsubscribe();
+	});
 </script>
 
 <Navbar />
